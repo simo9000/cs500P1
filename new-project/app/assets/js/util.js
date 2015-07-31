7cd456e706d9089208490cd18092f3f6fce57d63
simo9000 <alkylate2000@gmail.com>
2015-07-31 07:26:59 -0700

    updated schedule view to add new shift

--- new-project/app/assets/js/util.js
+++ new-project/app/assets/js/util.js
@@ -1,10 +1,28 @@
-function httpGet(theUrl)
+function httpGet(url)
 {
     var xmlHttp = null;
 
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.open( "GET", url, false );
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
 
+
+function httpPost(url, onSuccess)
+{
+	var xmlHttp = null;
+
+	xmlHttp = new XMLHttpRequest()
+	xmlHttp.open("POST", url, false);
+	xmlHttp.onreadystatechange=function()
+	{
+		if (xmlHttp.readyState == 4)
+			if (xmlHttp.status == 200)
+				onSuccess;
+			else
+				document.write(xmlHttp.responseText);
+	}
+	xmlHttp.send( null );
+	return xmlHttp.responseText;
+}
